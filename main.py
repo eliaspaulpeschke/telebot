79e8288788355758bba8a4e1a2b5f8fc39053060
Elias Peschke <eliasspeschke@googlemail.com>
2023-08-25 15:23:41 +0200

    commiiiiiitttttgit add .

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ import dotenv
 import telebot
 import requests
 import whisper
+import os
+import time
 
-
-
+currentfile = ""
 token = dotenv.get_key("./.env", "TOKEN")
 bot = telebot.TeleBot(token, parse_mode=None)
 model = whisper.load_model("base")
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start', 'help', "name"])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
-	
+    global currentfile
+    bot.reply_to(message, "Howdy, how are you doing?")
+    if message.text.startswith("/name"):
+        name=str(message.text)
+        name = name.removeprefix("/name")
+        name = name.strip()
+        name = name.replace(" ", "_")
+        name = name.casefold()
+        currentfile = name + ".md"
+        bot.reply_to(message, currentfile)
+        
 @bot.message_handler(content_types=["voice"])
 def echo_all(message):
     print(message.voice.file_id)
@@ -22,6 +32,14 @@ def echo_all(message):
     with open("res.ogg", "wb") as file:
         file.write(f.content)
     result = model.transcribe("res.ogg")
-    print(result["text"])
+    cwd = os.getcwd()
+    os.chdir("../General")
+    os.system("git pull")
+    with open(currentfile, "a") as file:
+        file.write(time.strftime("\n%d.%m.%y - %H:%M:\n"))
+        file.write(result["text"])
+    os.system("git add . && git commit -m 'update' && git push")
+    os.chdir(cwd)
+    bot.reply_to(message, f"Flename: {currentfile}\n\n{result['text']}")
 
 bot.infinity_polling()
