4f9ccd977bb1a11926eb45d79927a0e0e4b5140c
Elias Peschke <eliasspeschke@googlemail.com>
2023-08-25 17:51:31 +0200

    removed trash

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ Known Commands:
     set name of 
     current file 
     to append to
+/name [now|today]
+    sets the files
+    name to the current
+    date and time
 /id 
     check authorization
 /datemode [on|off]
@@ -48,20 +52,21 @@ file.
 @bot.message_handler(commands=["name"])
 def change_name(message):
     global state
-    if (message.text == "/name now" or message.text == "/name today"):
-        state.setfile(time.strftime("%d_%m_%y_%H_%M.md"))
-    else:
-        name = str(message.text)
-        name = name.removeprefix("/name")
-        name = name.strip()
-        name = name.casefold()
-        print(name)
-        if (not name.isspace()) and (not name == ""):
-            name = name.replace(" ", "_")
-            state.setfile(name + ".md")
+    if (str(message.from_user.id) in state.allowed_ids):
+        if (message.text == "/name now" or message.text == "/name today"):
+            state.setfile(time.strftime("%d_%m_%y_%H_%M.md"))
         else:
-            bot.reply_to(message, "Please give a filename - syntax:\n /name <filename>")
-    bot.reply_to(message, f"Current file name: {state.currentfile}")
+            name = str(message.text)
+            name = name.removeprefix("/name")
+            name = name.strip()
+            name = name.casefold()
+            print(name)
+            if (not name.isspace()) and (not name == ""):
+                name = name.replace(" ", "_")
+                state.setfile(name + ".md")
+            else:
+                bot.reply_to(message, "Please give a filename - syntax:\n /name <filename>")
+        bot.reply_to(message, f"Current file name: {state.currentfile}")
 
 @bot.message_handler(commands=['id'])
 def handle_id(message):
@@ -90,30 +95,33 @@ def handle_voicemsg(message):
 @bot.message_handler(content_types=['text'], commands=['save'])
 def handle_save(message):
     global state
-    text = message.text.removeprefix("/save")
-    text = state.handleText(text)
-    save_text(text)
-    bot.reply_to(message, f"Appendedd your text to {state.currentfile}")
+    if (str(message.from_user.id) in state.allowed_ids):
+        text = message.text.removeprefix("/save")
+        text = state.handleText(text)
+        save_text(text)
+        bot.reply_to(message, f"Appendedd your text to {state.currentfile}")
     
 @bot.message_handler(commands=['datemode'])
 def handle_date(message):
     global state
-    if(message.text == "/datemode"):
-        state.setdatemode(not state.datemode)
-    elif(message.text == "/datemode off"):
-        state.setdatemode(False)
-    elif(message.text == "/datemode off"):
-        state.setdatemode(True)
-    else:
-        bot.reply_to(message, "Usage: \n /datemode - toggle \n /datemode on - turn on \n /datemode off - turn off")
-    bot.reply_to(message, f"Datemode is {'on' if state.datemode == True else 'off'}")
+    if (str(message.from_user.id) in state.allowed_ids):
+        if(message.text == "/datemode"):
+            state.setdatemode(not state.datemode)
+        elif(message.text == "/datemode off"):
+            state.setdatemode(False)
+        elif(message.text == "/datemode off"):
+            state.setdatemode(True)
+        else:
+            bot.reply_to(message, "Usage: \n /datemode - toggle \n /datemode on - turn on \n /datemode off - turn off")
+        bot.reply_to(message, f"Datemode is {'on' if state.datemode == True else 'off'}")
     
 @bot.message_handler(commands=['ls'])
 def handle_ls(message):
     global state
-    files = os.listdir(state.repo_path)
-    files.sort()
-    bot.reply_to(message, "Files in your Repo: \n\n" + "\n".join(files))
+    if (str(message.from_user.id) in state.allowed_ids):
+       files = os.listdir(state.repo_path)
+       files.sort()
+       bot.reply_to(message, "Files in your Repo: \n\n" + "\n".join(files))
 
 def save_text(text):
         global state
